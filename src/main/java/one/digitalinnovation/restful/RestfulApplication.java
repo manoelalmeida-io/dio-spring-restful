578875bab1b81d1659ed4cb89a68c40fc8fb7792
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-02-25 00:07:18 -0300

    Configuração de ambiente

---
+++ src/main/java/one/digitalinnovation/restful/RestfulApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApplication.class, args);
+	}
+
+}
